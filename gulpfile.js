d53487ece58d35974c9de6b0fe6d624ff4f3b055
AxMari <AlexisMarien@gmail.com>
2016-06-24 12:54:21 -0400

    starting her up

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var browserSync = require("browser-sync").create();
+var jshint = require('gulp-jshint');
+
+
+gulp.task('default', ['browser-sync']);
+
+gulp.task('lint', function() {
+	return gulp.src(['map.js'
+		])
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('browser-sync', ['lint'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+
+    });
+    
+	gulp.watch(["**/*.html",
+				"**/*.css",
+				'**/*.js'])
+		.on('change', browserSync.reload);
+});	
