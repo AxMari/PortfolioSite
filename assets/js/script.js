1c2783586c2d45b47bcb49c8472f7a4d01d7f018
AxMari <AlexisMarien@gmail.com>
2016-06-27 22:44:28 -0400

    logo centered

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   particleground(document.getElementById('banner'), {
+//     dotColor: '#5cbdaa',
+//     lineColor: '#5cbdaa'
+//   })
+// });
